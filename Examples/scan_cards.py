577530136fdcbfcd3bfa9c73739741406b9057eb
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-02-09 15:04:21 +0530

    Update scan_cards.py

--- Examples/scan_cards.py
+++ Examples/scan_cards.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 # Basic exaple to read uhf cards
 import spidev as SPI
-import lcdLib_1inch14
-from PIL import Image,ImageDraw,ImageFont
 import mercury
 
 import RPi.GPIO as GPIO
@@ -10,37 +8,13 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-buzzer = 17
-GPIO.setup(buzzer,GPIO.OUT)
-
-
-
-Font1 = ImageFont.truetype("font/Font00.ttf",20)
-Font2 = ImageFont.truetype("font/Font00.ttf",16)
-Font3 = ImageFont.truetype("font/Font02.ttf",25)
-    
-
-disp = lcdLib_1inch14.lcd()
-disp.Init()
-image2 = Image.new("RGB", (disp.width, disp.height), "WHITE")
-
-
-image = Image.open('images/sb.jpg')	
-disp.Display_image(image)
-
-
-time.sleep(2)
-disp.clear()
-draw = ImageDraw.Draw(image2)
-
-draw.text((50,5),"SCAN THE CARDS", font = Font2, fill = "BLUE")
-disp.Display_image(image2)
-
-count = 30
+buzzer = 17 # connect buzzzer to GPIO17
+GPIO.setup(buzzer,GPIO.OUT)    
 
 reader = mercury.Reader("tmr:///dev/ttyS0")
 reader.set_region("EU3")
 reader.set_read_plan([1], "GEN2")
+time.sleep(2)
 
 try:
     while True:
@@ -56,12 +30,8 @@ try:
             x = x.lstrip('\'')
             print(x)
             
-            draw.text((10,count),x, font = Font2, fill = "RED")
-            disp.Display_image(image2)
-
             time.sleep(0.2)
             
-            count += 22
         else:
             GPIO.output(buzzer,GPIO.LOW)
 finally:
